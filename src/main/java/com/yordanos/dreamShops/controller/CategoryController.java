17794ef2982330f61f62a91b78fe2dc7e364e151
Yordanos Solomon <yordanos.sole@gmail.com>
2024-09-24 09:53:05 +0300

    Finishing Image Controller

--- src/main/java/com/yordanos/dreamShops/controller/CategoryController.java
+++ src/main/java/com/yordanos/dreamShops/controller/CategoryController.java
@@ -1,4 +1,34 @@
 package com.yordanos.dreamShops.controller;
 
+import com.yordanos.dreamShops.model.Category;
+import com.yordanos.dreamShops.response.ApiResponse;
+import com.yordanos.dreamShops.service.category.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found! ", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error: ", INTERNAL_SERVER_ERROR))
+        }
+    }
+
+
 }
--- src/main/java/com/yordanos/dreamShops/controller/ImageController.java
+++ src/main/java/com/yordanos/dreamShops/controller/ImageController.java
@@ -1,17 +1,21 @@
 package com.yordanos.dreamShops.controller;
 
 import com.yordanos.dreamShops.dto.ImageDto;
+import com.yordanos.dreamShops.exceptions.ResourceNotFoundException;
+import com.yordanos.dreamShops.model.Image;
 import com.yordanos.dreamShops.response.ApiResponse;
 import com.yordanos.dreamShops.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,10 +33,49 @@ public class ImageController {
         }
     }
 
-    @GetMapping("/api/vi/images/image/download/")
-    public ResponseEntity<ApiResponse> downloadImage (@PathVariable Long imageId) {
-        
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage (@PathVariable Long imageId, HttpEntity<Object> httpEntity) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int) image.getImage().length()));
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+                .body(resource);
     }
 
+    @PutMapping("/image/{imageId}/update")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("Update success!", image));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("Update failed!", NOT_FOUND));
+    }
 
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.deleteImageById(imageId);
+                return ResponseEntity.ok(new ApiResponse("Delete success!", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Delete failed!", INTERNAL_SERVER_ERROR));
+    }
 }
+
+
+
+
+
+
+
+
+
