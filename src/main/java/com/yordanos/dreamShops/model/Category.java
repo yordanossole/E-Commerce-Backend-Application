6a1e8679e9060c1b8cec502a42a4fa07c2652f8c
Yordanos Solomon <yordanos.sole@gmail.com>
2024-10-01 16:39:43 +0300

    Category entity relationship has been edited

--- src/main/java/com/yordanos/dreamShops/model/Category.java
+++ src/main/java/com/yordanos/dreamShops/model/Category.java
@@ -21,7 +21,7 @@ public class Category {
     private String name;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "category")
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products;
 
 
